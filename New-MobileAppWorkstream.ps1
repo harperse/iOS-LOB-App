05966ec855d9e5b0ef6c9a49053a946e82b37f0c
Sean Harper <seharper@microsoft.com>
2021-10-04 08:57:16 -0400

    Moved: Globals to main; Upload script as code

--- New-MobileAppWorkstream.ps1
+++ New-MobileAppWorkstream.ps1
@@ -6,6 +6,10 @@ param (
     [Parameter(Mandatory = $true)][string]$ApplicationName
 )
 
+#region Links
+# https://github.com/microsoftgraph/powershell-intune-samples/tree/master/LOB_Application
+#endregion Links
+
 #region Functions
 function Get-RandomPassword {
     #CODE COMPLETE
@@ -147,10 +151,61 @@ function New-AzAutomationRunAsAccount {
 
     return $ApplicationId
 }
+
+function CreateUploadScript {
+    $scriptBody = @"
+    
+    @("Az.Accounts", "Az.Storage") | ForEach-Object { Import-Module `$PSItem }
+    if (((Get-AzContext).Subscription.Id -ne "$subscriptionID") -or (Get-AzContext).Tenant.Id -ne "$tenantID") { 
+        Connect-AzAccount -Tenant "$tenantName" -SubscriptionId "$subscriptionID" -Verbose
+    }
+    if (!(Test-Path "$ApplicationName.ipa") -or (!(Test-Path "$ApplicationName.ipa.ps1"))) { 
+        Write-Error "Path specified not valid"; exit 
+    }
+    `$storageContext = (Get-AzStorageAccount -ResourceGroupName "$resourceGroupName" -Name "$storacctName").Context
+    `$storacctContainer = Get-AzStorageContainer -Context `$storageContext -Name "$storacctBlobName"
+    Get-ChildItem -Filter "$ApplicationName.*" | ForEach-Object { Set-AzStorageBlobContent -File `$PSItem -Container $($storacctContainer.Name) -Context `$storageContext -Force -BlobType Block }
+    
+    # Upload the .ipa and .ps1 files
+    Set-AzStorageBlobContent -File "$ApplicationName.ipa" -Container $($storacctContainer.Name) -Context `$storageContext -Force -BlobType Block
+    Set-AzStorageBlobContent -File "$ApplicationName.ipa.ps1" -Container $($storacctContainer.Name) -Context `$storageContext -Force -BlobType Block
+
+"@
+
+Set-Content -Path .\Upload-FileToAzBlob.ps1 -Value $scriptBody -Force
+
+} 
 #endregion Functions
 
 #region Main body
-. .\Globals.ps1 -ApplicationName $ApplicationName
+
+Clear-Host
+$Error.Clear()
+
+New-Variable -Name "tenantName" -Value "azinwcb089outlook.onmicrosoft.com" -Force
+New-Variable -Name "tenantID" -Value "40116f04-90e9-4f3d-a895-152754654561" -Force
+New-Variable -Name "subscriptionName" -Value "ES-CE-CUS-EXT-seharper" -Force
+New-Variable -Name "subscriptionID" -Value "709272e4-8b76-40d9-9a8a-00731f51eb83" -Force
+New-Variable -Name "location" -Value "eastus" -Force
+New-Variable -Name "azureEnvironment" -Value "AzureCloud" -Force
+
+New-Variable -Name "resourceGroupName" -Value $("rg", $ApplicationName.ToLower() -join $null) -Force
+New-Variable -Name "storacctName" -Value $("sa", $ApplicationName.ToLower() -join $null) -Force
+New-Variable -Name "storacctBlobName" -Value $("blob", $ApplicationName.ToLower() -join $null) -Force
+New-Variable -Name "aaAccountName" -Value $("aa", $ApplicationName.ToLower() -join $null) -Force
+New-Variable -Name "eventGridSubscriptionName" -Value $("es", $ApplicationName.ToLower() -join $null) -Force
+
+New-Variable -Name "aaRunbookName" -Value $("Publish", $ApplicationName.ToLower() -join "_") -Force
+New-Variable -Name "aaRunbookWebhookName" -Value "ExecutePipeline" -Force
+New-Variable -Name "CertificateAssetName" -Value  "AzureRunAsCertificate" -Force
+New-Variable -Name "ConnectionAssetName" -Value  "AzureRunAsConnection" -Force
+New-Variable -Name "ConnectionTypeName" -Value  "AzureServicePrincipal" -Force
+New-Variable -Name "certStore" -Value "cert:\LocalMachine\My" -Force
+New-Variable -Name "selfSignedCertNoOfMonthsUntilExpired" -Value 36 -Force
+New-Variable -Name "CertificateName" -Value $($aaAccountName, $CertificateAssetName -join $null) -Force
+New-Variable -Name "PfxCertPathForRunAsAccount" -Value $(Join-Path $env:TEMP ($CertificateName + ".pfx")) -Force
+New-Variable -Name "CerCertPathForRunAsAccount" -Value $(Join-Path $env:TEMP ($CertificateName + ".cer")) -Force
+
 $transcriptPath = ".\NewMobileAppWorkstream.$ApplicationName.log"
 if (Test-Path $transcriptPath) {
     Remove-Item $transcriptPath
@@ -214,14 +269,6 @@ $automationVariables = @{
 }
 
 $automationVariables.GetEnumerator().ForEach({ New-AzAutomationVariable -ResourceGroupName $resourceGroupName -AutomationAccountName $aaAccountName -Name $PSItem.Key -Value $PSItem.Value -Encrypted $false -Verbose })
-
-#New-AzAutomationVariable -ResourceGroupName $resourceGroupName -AutomationAccountName $aaAccountName -Name "StorageAccountKey1" -Value $storageAccountKeys[0].Value -Encrypted $false -Verbose
-#New-AzAutomationVariable -ResourceGroupName $resourceGroupName -AutomationAccountName $aaAccountName -Name "StorageAccountKey2" -Value $storageAccountKeys[1].Value -Encrypted $false -Verbose
-#New-AzAutomationVariable -ResourceGroupName $resourceGroupName -AutomationAccountName $aaAccountName -Name "ApplicationID" -Value $ApplicationServicePrincipal -Encrypted $false -Verbose
-#New-AzAutomationVariable -ResourceGroupName $resourceGroupName -AutomationAccountName $aaAccountName -Name "LOBType" -Value "microsoft.graph.iosLOBApp" -Encrypted $false -Verbose
-#New-AzAutomationVariable -ResourceGroupName $resourceGroupName -AutomationAccountName $aaAccountName -Name "CloudBlobContainer" -Value $storacctContainer.Name -Encrypted $false -Verbose
-#New-AzAutomationVariable -ResourceGroupName $resourceGroupName -AutomationAccountName $aaAccountName -Name "ApplicationName" -Value $ApplicationName -Encrypted $false -Verbose
-#New-AzAutomationVariable -ResourceGroupName $resourceGroupName -AutomationAccountName $aaAccountName -Name "ClientSecret" -Value $([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($ClientSecret))) -Encrypted $false -Verbose
 #endregion Prepare the automation account
 
 #region Prepare the event grid
--- Upload-FileToAzBlob.Manual.ps1
+++ Upload-FileToAzBlob.Manual.ps1
@@ -1,12 +1,16 @@
 [CmdletBinding()]
 param (
-    [Parameter()][string]$ApplicationName = "AnthemPulseV30"
+    [Parameter()][string]$ApplicationName = "AnthemPulse"
 )
 
-. .\Globals.ps1
+#. .\Globals.ps1
 @("Az.Accounts", "Az.Storage") | ForEach-Object { Import-Module $PSItem }
-if (((Get-AzContext).Subscription.Id -ne $subscriptionID) -or (Get-AzContext).Tenant.Id -ne $tenantID) { Connect-AzAccount -Tenant $tenantName -SubscriptionId $subscriptionID -Verbose }
-if (!(Test-Path "$ApplicationName.ipa") -or (!(Test-Path "$ApplicationName.ipa.ps1"))) { Write-Error "Path specified not valid"; exit }
+if (((Get-AzContext).Subscription.Id -ne $subscriptionID) -or (Get-AzContext).Tenant.Id -ne $tenantID) { 
+    Connect-AzAccount -Tenant $tenantName -SubscriptionId $subscriptionID -Verbose
+}
+if (!(Test-Path "$ApplicationName.ipa") -or (!(Test-Path "$ApplicationName.ipa.ps1"))) { 
+    Write-Error "Path specified not valid"; exit 
+}
 $storageContext = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storacctName).Context
 $storacctContainer = Get-AzStorageContainer -Context $storageContext -Name $storacctBlobName
 Get-ChildItem -Filter "$ApplicationName.*" | ForEach-Object { Set-AzStorageBlobContent -File $PSItem -Container $storacctContainer.Name -Context $storageContext -Force -BlobType Block }
@@ -15,5 +19,3 @@ Get-ChildItem -Filter "$ApplicationName.*" | ForEach-Object { Set-AzStorageBlobC
 Set-AzStorageBlobContent -File "$ApplicationName.ipa" -Container $storacctContainer.Name -Context $storageContext -Force -BlobType Block
 Set-AzStorageBlobContent -File "$ApplicationName.ipa.ps1" -Container $storacctContainer.Name -Context $storageContext -Force -BlobType Block
 
-#under globals . "$pwd\$ApplicationName.ipa.ps1"
-#GetiOSAppBody @appProperties | ConvertTo-Json | Set-Content .\$ApplicationName.ipa.json
